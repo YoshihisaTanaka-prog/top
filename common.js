371d3769f80cfbc205fff590f0e049ca1301b388
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-05-30 15:16:56 +0900

    GASのqueryに対応させた。
    ・GASのqueryの仕様を変更したのでそれに対応させた。
    ・GASのqueryへの通信テスト用のページを追加した。
    ・<h1>タグのデザインを中央揃えに変更

--- common.js
+++ common.js
@@ -1,34 +1,43 @@
 window.onload = function(){
     window.isLocal = false;
-        $.post('https://script.google.com/macros/s/AKfycbyA0gGkw67AJm8DxktH4zDeGWkDjLHftHWUMjn4LIDFls2KvejrGDw0Kf7GI9LpiBNyfw/exec', {'sheetname': 'top'}, function(data){
-        console.log(data);
-        for(var i = 0; i < data.length; i++){
-            var id = '#url-' + String(data[i].id);
-            console.log(id);
-            if (data[i].id == 104){
-                $.get(data[i].url, function(data, status, xhr){
-                    console.log(xhr.status)
-                    if(xhr.status == 200){
-                        isLocal = true;
-                    } else{
-                        isLocal = false;
-                    }
-                });
+    console.log('test');
+    $.post('https://script.google.com/macros/s/AKfycbyA0gGkw67AJm8DxktH4zDeGWkDjLHftHWUMjn4LIDFls2KvejrGDw0Kf7GI9LpiBNyfw/exec',
+        {'sheetname': 'top', 'mode': 'r'},
+        function(data){
+            console.log('test3');
+            console.log(data);
+            for(var i = 0; i < data.length; i++){
+                var id = '#url-' + String(data[i].vmId);
+                console.log(id);
+                if (data[i].vmId == 104){
+                    // ネットワークテスト用
+                    $.get(data[i].url, function(data, status, xhr){
+                        console.log(xhr.status)
+                        if(xhr.status == 200){
+                            isLocal = true;
+                        } else{
+                            isLocal = false;
+                        }
+                    });
+                }
+                if ($(id).length) {
+                    // リンク先の上書き
+                    $(id).each(function() {
+                        var url = data[i].url + $(this).attr('href');
+                        $(this).attr('href', url);
+                        console.log(url);
+                    });
+                }
             }
-            if ($(id).length) {
-                $(id).each(function() {
-                    var url = data[i].url + $(this).attr('href');
-                    $(this).attr('href', url);
-                    console.log(url);
-                });
+            if(loaded != null) {
+                if(typeof loaded == 'function'){
+                    setTimeout(() => {
+                        loaded();
+                    }, 2000);
+                }
             }
-        }
-        if(loaded != null) {
-            if(typeof loaded == 'function'){
-                setTimeout(() => {
-                    loaded();
-                }, 2000);
-            }
-        }
-    },'json');
+        }, 
+        'json'
+    );
+    console.log('test2');
 }
