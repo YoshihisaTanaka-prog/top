fe127f5d1251210a01901c62dece9f7d62e4eaea
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-07-21 10:22:55 +0900

    Update common.js

--- common.js
+++ common.js
@@ -5,37 +5,37 @@ window.onload = function(){
 
         console.log("https://yoshihiserver-playlist.herokuapp.com/channels.json", data);
     });
-    $.ajax('http://192.168.3.168', {dataType: 'html'}).done(function(data){
-        console.log(data);
-    }).fail(function(){
-        console.error('test failed');
-    });
-    $.post('https://script.google.com/macros/s/AKfycbyA0gGkw67AJm8DxktH4zDeGWkDjLHftHWUMjn4LIDFls2KvejrGDw0Kf7GI9LpiBNyfw/exec',
-        {'sheetname': 'top', 'mode': 'r'},
-        function(data){
-            console.log('test3');
-            console.log(data);
-            for(var i = 0; i < data.length; i++){
-                var id = '#url-' + String(data[i].vmId);
-                console.log(id);
-                if ($(id).length) {
-                    // リンク先の上書き
-                    $(id).each(function() {
-                        var url = data[i].url + $(this).attr('href');
-                        $(this).attr('href', url);
-                        console.log(url);
-                    });
-                }
-            }
-            if(loaded != null) {
-                if(typeof loaded == 'function'){
-                    setTimeout(() => {
-                        loaded();
-                    }, 2000);
-                }
-            }
-        }, 
-        'json'
-    );
+    // $.ajax('http://192.168.3.168', {dataType: 'html'}).done(function(data){
+    //     console.log(data);
+    // }).fail(function(){
+    //     console.error('test failed');
+    // });
+    // $.post('https://script.google.com/macros/s/AKfycbyA0gGkw67AJm8DxktH4zDeGWkDjLHftHWUMjn4LIDFls2KvejrGDw0Kf7GI9LpiBNyfw/exec',
+    //     {'sheetname': 'top', 'mode': 'r'},
+    //     function(data){
+    //         console.log('test3');
+    //         console.log(data);
+    //         for(var i = 0; i < data.length; i++){
+    //             var id = '#url-' + String(data[i].vmId);
+    //             console.log(id);
+    //             if ($(id).length) {
+    //                 // リンク先の上書き
+    //                 $(id).each(function() {
+    //                     var url = data[i].url + $(this).attr('href');
+    //                     $(this).attr('href', url);
+    //                     console.log(url);
+    //                 });
+    //             }
+    //         }
+    //         if(loaded != null) {
+    //             if(typeof loaded == 'function'){
+    //                 setTimeout(() => {
+    //                     loaded();
+    //                 }, 2000);
+    //             }
+    //         }
+    //     }, 
+    //     'json'
+    // );
     console.log('test2');
 }
